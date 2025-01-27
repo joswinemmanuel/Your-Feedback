d836f3fe4447eb993be1a0fbb829ee50512fc09e
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2025-01-27 19:16:31 +0530

    Javascript

---
+++ app.js
@@ -0,0 +1,34 @@
+// Getting Elements From The DOM
+const ratings = document.querySelectorAll(".rating");
+const ratingsContainer = document.querySelector(".ratings-container");
+const sendBtn = document.querySelector("#send");
+const panel = document.querySelector("#panel");
+
+let selectedRating = "";
+sendBtn.disabled = true;
+
+
+// Attaching Event
+ratingsContainer.addEventListener("click", (e) => {
+  if (e.target.parentNode.classList.contains("rating")) {
+    removeActive();
+    e.target.parentNode.classList.add("active");
+    selectedRating = e.target.nextElementSibling.innerHTML;
+    sendBtn.disabled = false;
+  }
+});
+
+sendBtn.addEventListener("click", () => {
+  panel.innerHTML = `
+    <p class="heart">💖</p>
+    <strong>Thank You! </strong>
+    <br>
+    <strong>Feedback: ${selectedRating} </strong>
+  `;
+});
+
+function removeActive() {
+  for (let i = 0; i < ratings.length; i++) {
+    ratings[i].classList.remove("active");
+  }
+}
